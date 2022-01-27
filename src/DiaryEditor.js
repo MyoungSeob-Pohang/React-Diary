3d393b2b414f342b7add7c06876862eb8960aa74
myoungseob <myoungseob91@gmail.com>
2022-01-27 23:28:58 +0900

    설명 추가

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,15 +1,28 @@
+// useRef 는 요소를 선택할 수 있는 주소값을 가진다.
+// useState는 상태를 관리할 수 있게 해준다.
+// 모두 react에 포함되어 있어서 함께 import
 import { useRef, useState } from 'react';
 
+// DiaryEditor 함수는 전체를 감싸는 함수 이다.
+// 파일명과 같으며 함수를 실행한다.
 const DiaryEditor = () => {
+    // 작성자 Ref
     const authorInput = useRef();
+    // 컨텐츠(일기내용) Ref
     const contentInput = useRef();
 
+    // 전체 상태관리를 위한 state
+    // 객체로 한번에 관리할 수 있다.
+    // key : value 값으로 관리하며 value는 초기값에 해당한다.
     const [state, setState] = useState({
         author: '',
         content: '',
         emotion: 1,
     });
 
+    // 이벤트 함수
+    // 스프레드로 전체 상태값을 받아오고 선택한 태그의 name값을 가져와서 대괄호 표기법으로 접근해서 값을 변경시킨다.
+    // name 값은 useState 의 선언된 key값과 동일함으로 접근 가능하다.
     const handleChangeState = (e) => {
         setState({
             ...state,
@@ -17,6 +30,8 @@ const DiaryEditor = () => {
         });
     };
 
+    // 클릭이벤트 함수
+    // 제목의 길이와 컨텐츠의 길이가 일정글자 수 이하이면 focus를 준다.
     const handleSubmit = (e) => {
         if (state.author.length < 1) {
             authorInput.current.focus();
@@ -31,6 +46,8 @@ const DiaryEditor = () => {
         alert('저장 성공');
     };
 
+    // 렌더링 되는 html
+    // state가 객체로 선언되어 있기 때문에 state.key 로 접근이 가능하다.
     return (
         <div className="DiaryEditor">
             <h2>오늘의 일기</h2>
